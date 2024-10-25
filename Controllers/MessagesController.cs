7fe68e5bd8274ba97b1766ca7b15c3cd9b57d78e
ilidance <144792172+ilidance@users.noreply.github.com>
2024-10-25 11:07:30 +0300

    Добавьте файлы проекта.

---
+++ Controllers/MessagesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+[ApiController]
+[Route("[controller]")]
+public class MessagesController : ControllerBase
+{
+    private static List<Message> messages = new List<Message>();
+
+    // отримати всі повідомлення 
+    [HttpGet]
+    public IEnumerable<Message> Get()
+    {
+        return messages;
+    }
+
+    // отримати нове повідомлення
+    [HttpPost]
+    public IActionResult Post([FromBody] Message message)
+    {
+        message.Id = messages.Count + 1;
+        message.Timestamp = DateTime.Now;
+        messages.Add(message);
+        return Ok(message);
+    }
+
+    // Отримання повідомлень для конкретного користувача
+    [HttpGet("{receiver}")]
+    public IEnumerable<Message> GetMessagesForUser(string receiver)
+    {
+        return messages.Where(m => m.Receiver == receiver);
+    }
+}
